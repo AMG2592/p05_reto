d9bd3f02e3b45b1b24fe787587030f695da4f049
amg2592 <almorga2592@gmail.com>
2022-10-05 18:55:17 -0500

    Se corrige tamaño de canvas

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const config = {
     type: Phaser.AUTO,		        //Tipo de renderizado (WEBGL, CANVAS, AUTO)
                                     // AUTO: busca primero WEBGL y si no está disponible eligirá CANVAS
     width: 2000,			            //Ancho de pantalla del juego
-    height: 1100, 			        //Alto de pantalla del juego
+    height: 940, 			        //Alto de pantalla del juego
     parent: "contenedor",		    //Nombre del id del elemento <div> en el index.html
                                     // se refiere a dónde se pondrá el canvas o lienzo
     pixelArt: true,		            //Diseño con pixeles definidos (no borrosos)
